c2450593c69cc121f393b6cda19b363c2ba8b6a2
Derrick <oseiagyemanb@gmail.com>
2024-04-06 23:06:44 +0000

    update of files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,15 +7,21 @@ const {
 	updateUser,
 	updateUserPassword,
 } = require('../controllers/userController');
+const {
+	authenticateUser,
+	authorizePermissions,
+} = require('../middleware/authentication');
 
 // route for get
-router.route('/').get(getAllUsers);
-router.route('/showUser').get(showCurrentUser);
+router
+	.route('/')
+	.get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers);
+router.route('/showUser').get(authenticateUser, showCurrentUser);
 // route for patch
-router.route('/updateUser').patch(updateUser);
-router.route('/updateUserPassword').patch(updateUserPassword);
+router.route('/updateUser').patch(authenticateUser, updateUser);
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
 
 // route for single user
-router.route('/:id').get(getSingleUser);
+router.route('/:id').get(authenticateUser, getSingleUser);
 
 module.exports = router;
