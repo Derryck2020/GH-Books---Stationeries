147fa94287f23022193b30c67f854370184fa54b
Derrick <oseiagyemanb@gmail.com>
2024-04-09 13:26:48 +0000

    update of file with checkPermission function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,11 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const {
+	createTokenUser,
+	attachCookiesToReponse,
+	checkPermissions,
+} = require('../utils');
 
 const getAllUsers = async (req, res) => {
 	const users = await User.find({ role: 'user' }).select('-password');
@@ -12,16 +17,31 @@ const getSingleUser = async (req, res) => {
 	if (!user) {
 		throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`);
 	}
-	// checkPermissions(req.user, user._id);
+	checkPermissions(req.user, user._id);
 	res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
 	res.status(StatusCodes.OK).json({ user: req.user });
 };
+
 const updateUser = async (req, res) => {
-	res.send(req.body);
+	const { email, name } = req.body;
+	if (!email || !name) {
+		throw new CustomError.BadRequestError('Please provide all values');
+	}
+	const user = await User.findOne({ _id: req.user.userId });
+
+	user.email = email;
+	user.name = name;
+
+	await user.save();
+
+	const tokenUser = createTokenUser(user);
+	attachCookiesToReponse({ res, user: tokenUser });
+	res.status(StatusCodes.OK).json({ user: tokenUser });
 };
+
 const updateUserPassword = async (req, res) => {
 	const { oldPassword, newPassword } = req.body;
 	if (!oldPassword || !newPassword) {
