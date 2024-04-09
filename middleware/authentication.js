61a6b6a1a317f22c78f9b93dfe56e3d033d23bf7
Derrick <oseiagyemanb@gmail.com>
2024-04-09 13:27:39 +0000

    update of file

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,11 +1,11 @@
-const customError = require('../errors');
+const CustomError = require('../errors');
 const { isTokenValid } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
 	const token = req.signedCookies.token;
 
 	if (!token) {
-		throw new customError.UnauthenticatedError('Authentication Invalid');
+		throw new CustomError.UnauthenticatedError('Authentication Invalid');
 	}
 
 	try {
@@ -13,14 +13,16 @@ const authenticateUser = async (req, res, next) => {
 		req.user = { name, userId, role };
 		next();
 	} catch (error) {
-		throw new customError.UnauthenticatedError('Authentication Invalid');
+		throw new CustomError.UnauthenticatedError('Authentication Invalid');
 	}
 };
 
 const authorizePermissions = (...roles) => {
 	return (req, res, next) => {
 		if (!roles.includes(req.user.role)) {
-			return res.status(403).json({ error: 'Unauthorized to access route' });
+			throw new CustomError.UnauthorizedError(
+				'Unauthorized to access this route'
+			);
 		}
 		next();
 	};
