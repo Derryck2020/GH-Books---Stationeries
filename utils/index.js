94508911f98d7226bfb20d0bbf70c5ca659fbdc9
Derrick <oseiagyemanb@gmail.com>
2024-04-13 21:49:49 +0000

    update of file

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,10 @@
-const { createJWT, isTokenValid } = require('./jwt');
-
+const { createJWT, isTokenValid, attachCookiesToReponse } = require('./jwt');
+const createTokenUser = require('./createTokenUser');
+const checkPermissions = require('./checkPermissions');
 module.exports = {
 	createJWT,
 	isTokenValid,
+	attachCookiesToReponse,
+	createTokenUser,
+	checkPermissions,
 };
--- utils/jwt.js
+++ utils/jwt.js
@@ -9,7 +9,21 @@ const createJWT = ({ payload }) => {
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
+const attachCookiesToReponse = ({ res, user }) => {
+	const token = createJWT({ payload: user });
+
+	const oneDay = 1000 * 60 * 24;
+
+	res.cookie('token', token, {
+		httpOnly: true,
+		expires: new Date(Date.now() + oneDay),
+		secure: process.env.NODE_ENV === 'proudction',
+		signed: true,
+	});
+};
+
 module.exports = {
 	createJWT,
 	isTokenValid,
+	attachCookiesToReponse,
 };
