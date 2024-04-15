ef9758a1bb08c9380de2f232f0def63a7d6267ef
Derrick <oseiagyemanb@gmail.com>
2024-04-15 07:24:31 +0000

    initial commit of file

--- models/Review.js
+++ models/Review.js
@@ -35,4 +35,49 @@ const ReviewSchema = mongoose.Schema(
 // We will create a compound index for user and product, so a user can only enter one review per product.
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+	const result = await this.aggregate([
+		{
+			$match: {
+				product: productId,
+			},
+		},
+		{
+			$group: {
+				_id: null,
+				averageRating: {
+					$avg: '$rating',
+				},
+				numOfReviews: {
+					$sum: 1,
+				},
+			},
+		},
+	]);
+	console.log(SpeechRecognitionResultList);
+	try {
+		await this.model('Product').findOneAndUpdate(
+			{ _id: productId },
+			{
+				averageRating: Math.ceil(result[0]?.averageRating || 0),
+				numOfReviews: result[0]?.numOfReviews || 0,
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+ReviewSchema.post('save', async function () {
+	await this.constructor.calculateAverageRating(this.product);
+});
+
+ReviewSchema.post(
+	'deleteOne',
+	{ document: true, query: false },
+	async function () {
+		console.log('post delete hook called');
+	}
+);
+
 module.exports = mongoose.model('Review', ReviewSchema);
