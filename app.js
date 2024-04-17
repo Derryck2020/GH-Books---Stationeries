25234e049010da7d168bb732b2273362a95101f5
Derrick <oseiagyemanb@gmail.com>
2024-04-17 09:52:54 +0000

    update of file

--- app.js
+++ app.js
@@ -28,19 +28,29 @@ const productRouter = require('./routes/productRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const orderRouter = require('./routes/orderRoutes');
 
-// middleware
+// middlewares
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000,
+		max: 60,
+	})
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+
 app.use(express.static('./public'));
 app.use(fileUpload({ useTempFiles: true }));
-
-app.get('/', (req, res) => {
-	res.send('Books Api');
-});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
